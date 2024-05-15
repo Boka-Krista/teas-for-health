be69e59aeac4a6943f74c7572dd34a225f08efe0
Krista Boka <bokakrista06@gmail.com>
2024-05-15 15:49:10 +0000

    Change translate buttons position

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        {pageLanguage: 'en'},
-        'google_translate_element'
-    );
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// Declare a loop variable
-var i;
-
-// List View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
