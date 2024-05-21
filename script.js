e3da8cc420d23101d607d68b4fdfa48983d72152
Krista Boka <bokakrista06@gmail.com>
2024-05-21 13:49:36 +0000

    Fix the layout

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var modal = document.getElementById("myModal");
+var modal = document.getElementById("clover");
+var modal = document.getElementById("lavander");
+var modal = document.getElementById("meadowsweet");
+var modal = document.getElementById("island-moss");
+var modal = document.getElementById("raspberry");
+var modal = document.getElementById("rosehip");
+var modal = document.getElementById("blackcurrant");
+var modal = document.getElementById("birch");
+var modal = document.getElementById("peppermint");
+var modal = document.getElementById("LINDEN-FLOWER");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -16,4 +25,24 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+}
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
 }
