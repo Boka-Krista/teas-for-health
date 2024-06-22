2c887015fdde63ed55910f8486871201b0e45241
Krista Boka <bokakrista06@gmail.com>
2024-06-22 18:57:21 +0000

    add modals to pages

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
+// Get the modal
 var modal = document.getElementById("myModal");
-var modal = document.getElementById("myModal1");
-var modal = document.getElementById("myModal2");
-var modal = document.getElementById("myModal3");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
-var img = document.getElementById("birch");
-var img = document.getElementById("blackcurrant");
-var img = document.getElementById("camomile");
-
 var modalImg = document.getElementById("img01");
-var modalImg = document.getElementById("img02");
-var modalImg = document.getElementById("img03");
-var modalImg = document.getElementById("img04");
-
 var captionText = document.getElementById("caption");
-var captionText = document.getElementById("caption1");
-var captionText = document.getElementById("caption2");
-var captionText = document.getElementById("caption3");
-
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-
-
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function() { 
   modal.style.display = "none";
 }
-
