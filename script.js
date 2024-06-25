30d3f32f504c94ad904ef2b673f1ef2231bbfb8f
Krista Boka <bokakrista06@gmail.com>
2024-06-25 15:22:59 +0000

    apply styles to modules

--- script.js
+++ script.js
@@ -3,28 +3,21 @@
 // Get all modals
 var modals = document.querySelectorAll(".modal");
 // Get all images
-var images = document.querySelectorAll(".img");
+var images = document.querySelectorAll("img");
 // Get all modal images
-var modalImages = document.querySelectorAll(".modal-img");
+var modalImages = document.querySelectorAll(".modal-content");
 // Get all caption texts
 var captionTexts = document.querySelectorAll(".caption");
 
-// Loop through each image and assign the click event
 images.forEach(function(image, index) {
+  image.style.cursor = "pointer";
   image.onclick = function() {
     modals[index].style.display = "block";
     modalImages[index].src = this.src;
     captionTexts[index].innerHTML = this.alt;
-  };
-});
-
-// Get all close buttons
-var closeButtons = document.querySelectorAll(".close");
-
-// Loop through each close button and assign the click event
-closeButtons.forEach(function(button) {
-  button.onclick = function() {
-    var modal = this.closest(".modal");
-    modal.style.display = "none";
+    /*
+     * SOMETHING LIKE THIS
+     */
+    captionTexts[index].style = "color:white;font-size:30rem";
   };
 });
